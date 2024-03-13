036c2e4a8c2ba9b2a2f4174186d3d7826d367bc5
21134lb <124744146+21134lb@users.noreply.github.com>
2024-03-13 13:25:44 +1300

    added the background of level
    added start and end point as well as walking area

--- game.js
+++ game.js
@@ -2,22 +2,49 @@
 // worlds hardest game clone
 /*******************************************************/
 let currentScreen = 'game'
-let PLAYERSPEED = 0;
+const PLAYERSPEED = 3;
 /*******************************************************/
 // setup()
 /*******************************************************/
 
 function setup() {
-    cnv = new Canvas('2:1');
+    cnv = new Canvas(800,400,'fullscreen');
     
-    const PLAYERWIDTH = width/36;
-    const PLAYERHEIGHT = height/18;
-    const PLAYERSPAWNY = height/2;
-    const PLAYERSPAWNX = width/8;
-    PLAYERSPEED = height/144;
+    const PLAYERWIDTH = 22;
+    const PLAYERHEIGHT = 22;
+    const PLAYERSPAWNY = 200;
+    const PLAYERSPAWNX = 100;
+    
+    const SPAWNPOINTWIDTH = 114;
+    const SPAWNPOINTHEIGHT = 133;
+    const SPAWNPOINTY = 200;
+    const SPAWNPOINTX = 100;
+    
+    const WALKINGAREAWIDTH = 400;
+    const WALKINGAREAHEIGHT = 266;
+    const WALKINGAREAY = 200;
+    const WALKINGAREAX = 400;
+    
+    const ENDPOINTWIDTH = 114
+    const ENDPOINTHEIGHT = 133
+    const ENDPOINTY = 200
+    const ENDPOINTX = 700
+    
+    bridgeArea = new Sprite(400, 200, 500, 70, 'n');
+    bridgeArea.color = 'white'
+    
+    walkingArea = new Sprite(WALKINGAREAX, WALKINGAREAY, WALKINGAREAWIDTH, WALKINGAREAHEIGHT, 'n');
+    walkingArea.color = 'white'
+    
+    spawnPoint = new Sprite(SPAWNPOINTX, SPAWNPOINTY, SPAWNPOINTWIDTH, SPAWNPOINTHEIGHT, 'n');
+    spawnPoint.color = 'lightgreen'
+    
+    endPoint = new Sprite(ENDPOINTX, ENDPOINTY, ENDPOINTWIDTH, ENDPOINTHEIGHT, 'n');
+    endPoint.color = 'lightgreen'
     
     player = new Sprite(PLAYERSPAWNX, PLAYERSPAWNY , PLAYERWIDTH, PLAYERHEIGHT, 'd');
     player.color = 'red';
+    
 }
 
 /*******************************************************/
